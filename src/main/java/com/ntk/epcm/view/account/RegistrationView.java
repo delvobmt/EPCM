df305e27e2295951eb35d5143ac2779c6ff92bbc
KhoaNT <delvobmt@gmail.com>
2016-09-21 23:33:30 +0700

    account

--- src/main/java/com/ntk/epcm/view/account/RegistrationView.java
+++ src/main/java/com/ntk/epcm/view/account/RegistrationView.java
@@ -5,13 +5,11 @@ import javax.faces.bean.ManagedBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ntk.epcm.service.AccountService;
+
 @ManagedBean
 public class RegistrationView {
-	
+
 	@Autowired
 	AccountService accountService;
-	
-	public String getTitle(){
-		return "Registration";
-	}
+
 }
