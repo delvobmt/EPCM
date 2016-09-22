f7bb2af49a53cc0ef815a3b06741c6d5fc98bebb
KhoaNT <delvobmt@gmail.com>
2016-09-22 23:14:38 +0700

    acounting

--- src/main/java/com/ntk/epcm/view/account/RegistrationView.java
+++ src/main/java/com/ntk/epcm/view/account/RegistrationView.java
@@ -5,11 +5,17 @@ import javax.faces.bean.ManagedBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ntk.epcm.service.AccountService;
+import javax.validation.constraints.Size;
 
 @ManagedBean
 public class RegistrationView {
 
 	@Autowired
 	AccountService accountService;
+	
+	@Size
+	private String username;
+	private String name;
+	private String email;
 
 }
